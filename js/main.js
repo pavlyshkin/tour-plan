03f3328e23ede9684a5cc18aea9f19d7b0280962
pavlyshkin <pasch.fedorow2013@yandex.ru>
2021-03-10 14:11:43 +0300

    create: API ya.karts

--- js/main.js
+++ js/main.js
@@ -13,3 +13,11 @@ const swiper = new Swiper('.swiper-container', {
   },
 
 });
+
+ymaps.ready(init);
+function init(){
+  var myMap = new ymaps.Map('map',{
+    center: [7.60275901, 79.79381638],
+    zoom: 13
+  });
+}

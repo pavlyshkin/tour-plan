b025db5315fe8c8b4d9ae23ea92ac75aebc3ff7e
pavlyshkin <pasch.fedorow2013@yandex.ru>
2021-03-22 21:17:57 +0300

    create: added modal

--- js/main.js
+++ js/main.js
@@ -1,34 +1,59 @@
-const hotelSlider = new Swiper('.hotel-slider', {
-  // Optional parameters
-  loop: true,
+$(document).ready(function () {
+    const hotelSlider = new Swiper('.hotel-slider', {
+    // Optional parameters
+    loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
-  },
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-  effect: "coverflow"
+    // Navigation arrows
+    navigation: {
+      nextEl: '.hotel-slider__button--next',
+      prevEl: '.hotel-slider__button--prev',
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+    effect: "coverflow"
+  });
+
+  const reviewsSlider = new Swiper('.reviews-slider', {
+    // Optional parameters
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.reviews-slider__button--next',
+      prevEl: '.reviews-slider__button--prev',
+    },
+
+  });
+
+
+  var menuButton = $(".menu-button")
+  menuButton.on('click', function (){
+    $(".navbar-bottom").toggleClass('navbar-bottom--visible')
+  });
+
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  modalButton.on('click',openModal);
+  closeModalButton.on('click', closeModal)
+
+  function openModal() {
+    var targetModal = $(this).attr('data-href');
+    $(targetModal).find('.modal__overlay').addClass("modal__overlay--visible");
+    $(targetModal).find('.modal__dialog').addClass("modal__dialog--visible");
+  }
+  function closeModal (event) {
+  event.preventDefold()
+  var modalOverlay = $(".modal__overlay");
+  var modalDialog = $(".modal__dialog");
+  modalDialog.closeClass('modal__dialog--visible');
+  modalOverlay.closeClass('modal__overlay--visible');
+  }
+  $(document).keydown(function(event) { 
+  if (event.keyCode == 27) { 
+    $('.modal__overlay').hide();
+    $('.modal__dialog').hide();
+  }
 });
-
-const reviewsSlider = new Swiper('.reviews-slider', {
-  // Optional parameters
-  loop: true,
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.reviews-slider__button--next',
-    prevEl: '.reviews-slider__button--prev',
-  },
-
-});
-
-
-var menuButton = document.querySelector(".menu-button")
-menuButton.addEventListener('click', function (){
-  console.log("клик по кномке меню");
-  document.querySelector(".navbar-bottom").classList.toggle('navbar-bottom--visible')
 });
